1f3850b67c685a497e366af677c161868b83af4b
Catherine Safrit <34667751+csafrit@users.noreply.github.com>
2020-09-17 14:06:21 -0400

    Add files via upload

---
+++ worldbank.js
@@ -0,0 +1,923 @@
+var countries = [
+  {
+    "Country": "Argentina",
+    "CountryCode": "AR",
+    "Type": "Rosé",
+    "Count": 1,
+    "Price": 11.99,
+    "Rating": 7.4
+  },
+  {
+    "Country": "Argentina",
+    "CountryCode": "AR2",
+    "Type": "Red Wine",
+    "Count": 65,
+    "Price": 21.22,
+    "Rating": 7.88
+  },
+  {
+    "Country": "Argentina",
+    "CountryCode": "AR3",
+    "Type": "Champagne",
+    "Count": 300,
+    "Price": 16.32,
+    "Rating": 8.4
+  },
+  {
+    "Country": "Argentina",
+    "CountryCode": "AR4",
+    "Type": "White Wine",
+    "Count": 6,
+    "Price": 16.99,
+    "Rating": 7.74
+  },
+  {
+    "Country": "Australia",
+    "CountryCode": "AU",
+    "Type": "Red Wine",
+    "Count": 47,
+    "Price": 42.78,
+    "Rating": 8.08
+  },
+  {
+    "Country": "Australia",
+    "CountryCode": "AU2",
+    "Type": "White Wine",
+    "Count": 8,
+    "Price": 44.21,
+    "Rating": 7.62
+  },
+  {
+    "Country": "Australia",
+    "CountryCode": "AU3",
+    "Type": "Champagne",
+    "Count": 81,
+    "Price": 54.49,
+    "Rating": 8.98
+  },
+  {
+    "Country": "Finland",
+    "CountryCode": "FN",
+    "Type": "Red Wine",
+    "Count": 18,
+    "Price": 32.17,
+    "Rating": 8.1
+  },
+  {
+    "Country": "Finland",
+    "CountryCode": "FN2",
+    "Type": "White Wine",
+    "Count": 6,
+    "Price": 18.32,
+    "Rating": 8.26
+  },
+  {
+    "Country": "South Korea",
+    "CountryCode": "SK",
+    "Type": "Red Wine",
+    "Count": 4,
+    "Price": 13.5,
+    "Rating": 8.15
+  },
+  {
+    "Country": "Austria",
+    "CountryCode": "AT",
+    "Type": "White Wine",
+    "Count": 1,
+    "Price": 14.99,
+    "Rating": 8
+  },
+  {
+    "Country": "Austria",
+    "CountryCode": "AT2",
+    "Type": "Champagne",
+    "Count": 3,
+    "Price": 23,
+    "Rating": 8.8
+  },
+  {
+    "Country": "Chile",
+    "CountryCode": "CL",
+    "Type": "Red Wine",
+    "Count": 34,
+    "Price": 28.84,
+    "Rating": 7.96
+  },
+  {
+    "Country": "Chile",
+    "CountryCode": "CL2",
+    "Type": "White Wine",
+    "Count": 6,
+    "Price": 16.66,
+    "Rating": 7.4
+  },
+  {
+    "Country": "Chile",
+    "CountryCode": "CL3",
+    "Type": "Champagne",
+    "Count": 1,
+    "Price": 12,
+    "Rating": 8.1
+  },
+  {
+    "Country": "France",
+    "CountryCode": "FRA",
+    "Type": "Rosé",
+    "Count": 27,
+    "Price": 16.58,
+    "Rating": 8.22
+  },
+  {
+    "Country": "France",
+    "CountryCode": "FRA2",
+    "Type": "Red Wine",
+    "Count": 194,
+    "Price": 87.5,
+    "Rating": 7.98
+  },
+  {
+    "Country": "France",
+    "CountryCode": "FRA3",
+    "Type": "Champagne",
+    "Count": 487,
+    "Price": 83.3,
+    "Rating": 8.9
+  },
+  {
+    "Country": "France",
+    "CountryCode": "FRA4",
+    "Type": "White Wine",
+    "Count": 41,
+    "Price": 23.14,
+    "Rating": 7.8
+  },
+  {
+    "Country": "Germany",
+    "CountryCode": "DE",
+    "Type": "White Wine",
+    "Count": 10,
+    "Price": 18.89,
+    "Rating": 8.26
+  },
+  {
+    "Country": "Greece",
+    "CountryCode": "GR",
+    "Type": "Red Wine",
+    "Count": 2,
+    "Price": 27.49,
+    "Rating": 9.2
+  },
+  {
+    "Country": "Greece",
+    "CountryCode": "GR2",
+    "Type": "White Wine",
+    "Count": 2,
+    "Price": 17.49,
+    "Rating": 6.7
+  },
+  {
+    "Country": "Italy",
+    "CountryCode": "IT",
+    "Type": "Rosé",
+    "Count": 2,
+    "Price": 14.99,
+    "Rating": 8
+  },
+  {
+    "Country": "Italy",
+    "CountryCode": "IT2",
+    "Type": "Red Wine",
+    "Count": 198,
+    "Price": 39.5,
+    "Rating": 8.12
+  },
+  {
+    "Country": "Italy",
+    "CountryCode": "IT3",
+    "Type": "Champagne",
+    "Count": 70,
+    "Price": 17.12,
+    "Rating": 8.36
+  },
+  {
+    "Country": "Italy",
+    "CountryCode": "IT4",
+    "Type": "White Wine",
+    "Count": 57,
+    "Price": 16.51,
+    "Rating": 8.26
+  },
+  {
+    "Country": "Languedoc-Roussillon",
+    "CountryCode": "LR",
+    "Type": "Champagne",
+    "Count": 4,
+    "Price": 19.24,
+    "Rating": 8.9
+  },
+  {
+    "Country": "Loire",
+    "CountryCode": "LO",
+    "Type": "Red Wine",
+    "Count": 1,
+    "Price": 25.99,
+    "Rating": 7.8
+  },
+  {
+    "Country": "Loire",
+    "CountryCode": "LO2",
+    "Type": "White Wine",
+    "Count": 5,
+    "Price": 34.59,
+    "Rating": 8.8
+  },
+  {
+    "Country": "New Zealand",
+    "CountryCode": "NZ",
+    "Type": "Rosé",
+    "Count": 2,
+    "Price": 13.49,
+    "Rating": 8
+  },
+  {
+    "Country": "New Zealand",
+    "CountryCode": "NZ2",
+    "Type": "Red Wine",
+    "Count": 8,
+    "Price": 31.98,
+    "Rating": 7.52
+  },
+  {
+    "Country": "New Zealand",
+    "CountryCode": "NZ3",
+    "Type": "White Wine",
+    "Count": 31,
+    "Price": 16.18,
+    "Rating": 8.32
+  },
+  {
+    "Country": "New Zealand",
+    "CountryCode": "NZ4",
+    "Type": "Champagne",
+    "Count": 5,
+    "Price": 18.33,
+    "Rating": 8.74
+  },
+  {
+    "Country": "Switzerland",
+    "CountryCode": "SZ",
+    "Type": "Red Wine",
+    "Count": 2,
+    "Price": 28.5,
+    "Rating": 8.45
+  },
+  {
+    "Country": "Switzerland",
+    "CountryCode": "SZ2",
+    "Type": "White Wine",
+    "Count": 2,
+    "Price": 24.5,
+    "Rating": 9
+  },
+  {
+    "Country": "Turkey",
+    "CountryCode": "TY",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 15.67,
+    "Rating": 8.76
+  },
+  {
+    "Country": "Turkey",
+    "CountryCode": "TY2",
+    "Type": "Red Wine",
+    "Count": 39,
+    "Price": 29.08,
+    "Rating": 8.84
+  },
+  {
+    "Country": "Turkey",
+    "CountryCode": "TY3",
+    "Type": "White Wine",
+    "Count": 10,
+    "Price": 16.7,
+    "Rating": 8.72
+  },
+  {
+    "Country": "Ukraine",
+    "CountryCode": "UE",
+    "Type": "Rosé",
+    "Count": 1,
+    "Price": 13,
+    "Rating": 8.6
+  },
+  {
+    "Country": "Ukraine",
+    "CountryCode": "UE2",
+    "Type": "Rosé",
+    "Count": 4,
+    "Price": 13,
+    "Rating": 8.42
+  },
+  {
+    "Country": "Sweden",
+    "CountryCode": "NC",
+    "Type": "Red Wine",
+    "Count": 7,
+    "Price": 19.13,
+    "Rating": 8.06
+  },
+  {
+    "Country": "Sweden",
+    "CountryCode": "NC2",
+    "Type": "White Wine",
+    "Count": 2,
+    "Price": 13.99,
+    "Rating": 7.4
+  },
+  {
+    "Country": "Portugal",
+    "CountryCode": "PT",
+    "Type": "Rosé",
+    "Count": 2,
+    "Price": 9.99,
+    "Rating": 7.5
+  },
+  {
+    "Country": "Portugal",
+    "CountryCode": "PT2",
+    "Type": "Red Wine",
+    "Count": 1,
+    "Price": 23.99,
+    "Rating": 8
+  },
+  {
+    "Country": "Portugal",
+    "CountryCode": "PT3",
+    "Type": "White Wine",
+    "Count": 4,
+    "Price": 9.24,
+    "Rating": 7.9
+  },
+  {
+    "Country": "South Africa",
+    "CountryCode": "ZA",
+    "Type": "Red Wine",
+    "Count": 121,
+    "Price": 24.58,
+    "Rating": 8.75
+  },
+  {
+    "Country": "South Africa",
+    "CountryCode": "ZA2",
+    "Type": "White Wine",
+    "Count": 96,
+    "Price": 16.99,
+    "Rating": 8.2
+  },
+  {
+    "Country": "South Africa",
+    "CountryCode": "ZA3",
+    "Type": "Rosé",
+    "Count": 26,
+    "Price": 14,
+    "Rating": 8.6
+  },
+  {
+    "Country": "South Africa",
+    "CountryCode": "ZA4",
+    "Type": "Champagne",
+    "Count": 12,
+    "Price": 16.58,
+    "Rating": 8.63
+  },
+  {
+    "Country": "Brazil",
+    "CountryCode": "BR",
+    "Type": "Red Wine",
+    "Count": 20,
+    "Price": 20,
+    "Rating": 8.2
+  },
+  {
+    "Country": "Brazil",
+    "CountryCode": "BR2",
+    "Type": "White Wine",
+    "Count": 5,
+    "Price": 19.6,
+    "Rating": 8.3
+  },
+  {
+    "Country": "Bulgaria",
+    "CountryCode": "BG",
+    "Type": "Red Wine",
+    "Count": 46,
+    "Price": 13.17,
+    "Rating": 8.6
+  },
+  {
+    "Country": "Bulgaria",
+    "CountryCode": "BG2",
+    "Type": "White Wine",
+    "Count": 30,
+    "Price": 9.13,
+    "Rating": 8.47
+  },
+  {
+    "Country": "Canada",
+    "CountryCode": "CN",
+    "Type": "White Wine",
+    "Count": 139,
+    "Price": 29.66,
+    "Rating": 8.8
+  },
+  {
+    "Country": "Canada",
+    "CountryCode": "CN2",
+    "Type": "Red Wine",
+    "Count": 54,
+    "Price": 48.03,
+    "Rating": 8.9
+  },
+  {
+    "Country": "Canada",
+    "CountryCode": "CN3",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 20,
+    "Rating": 8.87
+  },
+  {
+    "Country": "China",
+    "CountryCode": "CH",
+    "Type": "White Wine",
+    "Count": 3,
+    "Price": 20.33,
+    "Rating": 8.2
+  },
+  {
+    "Country": "Croatia",
+    "CountryCode": "CO",
+    "Type": "White Wine",
+    "Count": 49,
+    "Price": 19.23,
+    "Rating": 8.59
+  },
+  {
+    "Country": "Croatia",
+    "CountryCode": "CO2",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 21,
+    "Rating": 8.63
+  },
+  {
+    "Country": "Croatia",
+    "CountryCode": "CO3",
+    "Type": "Red Wine",
+    "Count": 37,
+    "Price": 27.78,
+    "Rating": 8.67
+  },
+  {
+    "Country": "Cyprus",
+    "CountryCode": "CY",
+    "Type": "Red Wine",
+    "Count": 23,
+    "Price": 15.74,
+    "Rating": 8.59
+  },
+  {
+    "Country": "Cyprus",
+    "CountryCode": "CY2",
+    "Type": "White Wine",
+    "Count": 8,
+    "Price": 14.75,
+    "Rating": 8.56
+  },
+  {
+    "Country": "Czech Republic",
+    "CountryCode": "CZ",
+    "Type": "White Wine",
+    "Count": 4,
+    "Price": 18.75,
+    "Rating": 8.6
+  },
+  {
+    "Country": "Czech Republic",
+    "CountryCode": "CZ2",
+    "Type": "Red Wine",
+    "Count": 2,
+    "Price": 16.5,
+    "Rating": 8.55
+  },
+  {
+    "Country": "Egypt",
+    "CountryCode": "EG",
+    "Type": "Red Wine",
+    "Count": 1,
+    "Price": 14.75,
+    "Rating": 8.25
+  },
+  {
+    "Country": "England",
+    "CountryCode": "EN",
+    "Type": "Rosé",
+    "Count": 1,
+    "Price": 44,
+    "Rating": 9.4
+  },
+  {
+    "Country": "England",
+    "CountryCode": "EN2",
+    "Type": "White Wine",
+    "Count": 8,
+    "Price": 48,
+    "Rating": 9.28
+  },
+  {
+    "Country": "Georgia",
+    "CountryCode": "GA",
+    "Type": "White Wine",
+    "Count": 26,
+    "Price": 18.88,
+    "Rating": 8.63
+  },
+  {
+    "Country": "Georgia",
+    "CountryCode": "GA2",
+    "Type": "Red Wine",
+    "Count": 17,
+    "Price": 18.12,
+    "Rating": 8.43
+  },
+  {
+    "Country": "Hungary",
+    "CountryCode": "HY",
+    "Type": "Red Wine",
+    "Count": 82,
+    "Price": 36.67,
+    "Rating": 8.56
+  },
+  {
+    "Country": "Hungary",
+    "CountryCode": "HY2",
+    "Type": "Rosé",
+    "Count": 4,
+    "Price": 13.5,
+    "Rating": 8.5
+  },
+  {
+    "Country": "Hungary",
+    "CountryCode": "HY3",
+    "Type": "White Wine",
+    "Count": 145,
+    "Price": 49.35,
+    "Rating": 8.83
+  },
+  {
+    "Country": "India",
+    "CountryCode": "IN",
+    "Type": "White Wine",
+    "Count": 4,
+    "Price": 11.5,
+    "Rating": 8.9
+  },
+  {
+    "Country": "India",
+    "CountryCode": "IN2",
+    "Type": "Red Wine",
+    "Count": 4,
+    "Price": 16.25,
+    "Rating": 8.63
+  },
+  {
+    "Country": "Israel",
+    "CountryCode": "IS",
+    "Type": "Red Wine",
+    "Count": 479,
+    "Price": 34.98,
+    "Rating": 8.74
+  },
+  {
+    "Country": "Israel",
+    "CountryCode": "IS2",
+    "Type": "Rosé",
+    "Count": 11,
+    "Price": 19.09,
+    "Rating": 8.6
+  },
+  {
+    "Country": "Israel",
+    "CountryCode": "IS3",
+    "Type": "White Wine",
+    "Count": 140,
+    "Price": 20.08,
+    "Rating": 8.66
+  },
+  {
+    "Country": "Israel",
+    "CountryCode": "IS4",
+    "Type": "Champagne",
+    "Count": 2,
+    "Price": 24,
+    "Rating": 8.8
+  },
+  {
+    "Country": "Japan",
+    "CountryCode": "JP",
+    "Type": "Red Wine",
+    "Count": 2,
+    "Price": 24,
+    "Rating": 8.5
+  },
+  {
+    "Country": "Lebanon",
+    "CountryCode": "LB",
+    "Type": "Red Wine",
+    "Count": 21,
+    "Price": 25,
+    "Rating": 8.53
+  },
+  {
+    "Country": "Lebanon",
+    "CountryCode": "LB2",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 18.3,
+    "Rating": 8.73
+  },
+  {
+    "Country": "Lebanon",
+    "CountryCode": "LB3",
+    "Type": "White Wine",
+    "Count": 13,
+    "Price": 27.77,
+    "Rating": 8.6
+  },
+  {
+    "Country": "Lithuania",
+    "CountryCode": "LI",
+    "Type": "White Wine",
+    "Count": 8,
+    "Price": 10,
+    "Rating": 8.42
+  },
+  {
+    "Country": "Uruguay",
+    "CountryCode": "UY",
+    "Type": "White Wine",
+    "Count": 11,
+    "Price": 22.09,
+    "Rating": 8.43
+  },
+  {
+    "Country": "Uruguay",
+    "CountryCode": "UY2",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 15,
+    "Rating": 8
+  },
+  {
+    "Country": "Uruguay",
+    "CountryCode": "UY3",
+    "Type": "Red Wine",
+    "Count": 78,
+    "Price": 26.88,
+    "Rating": 8.46
+  },
+  {
+    "Country": "Luxembourg",
+    "CountryCode": "LX",
+    "Type": "White Wine",
+    "Count": 9,
+    "Price": 40.67,
+    "Rating": 8.7
+  },
+  {
+    "Country": "Macedonia",
+    "CountryCode": "MC",
+    "Type": "White Wine",
+    "Count": 2,
+    "Price": 14,
+    "Rating": 8.7
+  },
+  {
+    "Country": "Macedonia",
+    "CountryCode": "MC2",
+    "Type": "Red Wine",
+    "Count": 14,
+    "Price": 15.5,
+    "Rating": 8.45
+  },
+  {
+    "Country": "Mexico",
+    "CountryCode": "MX",
+    "Type": "Red Wine",
+    "Count": 42,
+    "Price": 33.76,
+    "Rating": 8.52
+  },
+  {
+    "Country": "Mexico",
+    "CountryCode": "MX2",
+    "Type": "Rosé",
+    "Count": 2,
+    "Price": 23,
+    "Rating": 8.25
+  },
+  {
+    "Country": "Mexico",
+    "CountryCode": "MX3",
+    "Type": "White Wine",
+    "Count": 19,
+    "Price": 19.42,
+    "Rating": 8.39
+  },
+  {
+    "Country": "Moldova",
+    "CountryCode": "ML",
+    "Type": "White Wine",
+    "Count": 28,
+    "Price": 14.35,
+    "Rating": 8.44
+  },
+  {
+    "Country": "Moldova",
+    "CountryCode": "ML2",
+    "Type": "Red Wine",
+    "Count": 43,
+    "Price": 16.02,
+    "Rating": 8.49
+  },
+  {
+    "Country": "Moldova",
+    "CountryCode": "ML3",
+    "Type": "Champagne",
+    "Count": 2,
+    "Price": 12,
+    "Rating": 8.3
+  },
+  {
+    "Country": "Montenegro",
+    "CountryCode": "MT",
+    "Type": "Red Wine",
+    "Count": 2,
+    "Price": 10,
+    "Rating": 8.2
+  },
+  {
+    "Country": "Morocco",
+    "CountryCode": "MO",
+    "Type": "Red Wine",
+    "Count": 8,
+    "Price": 20,
+    "Rating": 8.85
+  },
+  {
+    "Country": "Morocco",
+    "CountryCode": "MO2",
+    "Type": "White Wine",
+    "Count": 4,
+    "Price": 16.5,
+    "Rating": 8.75
+  },
+  {
+    "Country": "Romania",
+    "CountryCode": "RM",
+    "Type": "White Wine",
+    "Count": 63,
+    "Price": 16.68,
+    "Rating": 8.46
+  },
+  {
+    "Country": "Romania",
+    "CountryCode": "RM2",
+    "Type": "Rosé",
+    "Count": 1,
+    "Price": 9,
+    "Rating": 8.8
+  },
+  {
+    "Country": "Romania",
+    "CountryCode": "RM3",
+    "Type": "Red Wine",
+    "Count": 75,
+    "Price": 16.25,
+    "Rating": 8.51
+  },
+  {
+    "Country": "Serbia",
+    "CountryCode": "SB",
+    "Type": "Red Wine",
+    "Count": 8,
+    "Price": 26.75,
+    "Rating": 8.78
+  },
+  {
+    "Country": "Serbia",
+    "CountryCode": "SB2",
+    "Type": "White Wine",
+    "Count": 6,
+    "Price": 21,
+    "Rating": 8.75
+  },
+  {
+    "Country": "Slovakia",
+    "CountryCode": "SV",
+    "Type": "White Wine",
+    "Count": 3,
+    "Price": 15.33,
+    "Rating": 8.36
+  },
+  {
+    "Country": "Slovenia",
+    "CountryCode": "SL",
+    "Type": "White Wine",
+    "Count": 62,
+    "Price": 25.88,
+    "Rating": 8.8
+  },
+  {
+    "Country": "Slovenia",
+    "CountryCode": "SL2",
+    "Type": "Red Wine",
+    "Count": 29,
+    "Price": 32.92,
+    "Rating": 8.88
+  },
+  {
+    "Country": "Slovenia",
+    "CountryCode": "SL3",
+    "Type": "Rosé",
+    "Count": 3,
+    "Price": 23.67,
+    "Rating": 8.73
+  },
+  {
+    "Country": "Spain",
+    "CountryCode": "ES",
+    "Type": "Rosé",
+    "Count": 2,
+    "Price": 12.99,
+    "Rating": 6.8
+  },
+  {
+    "Country": "Spain",
+    "CountryCode": "ES2",
+    "Type": "Red Wine",
+    "Count": 83,
+    "Price": 34,
+    "Rating": 8.08
+  },
+  {
+    "Country": "Spain",
+    "CountryCode": "ES3",
+    "Type": "Champagne",
+    "Count": 18,
+    "Price": 13.71,
+    "Rating": 7.9
+  },
+  {
+    "Country": "Spain",
+    "CountryCode": "ES4",
+    "Type": "White Wine",
+    "Count": 13,
+    "Price": 15.53,
+    "Rating": 7.96
+  },
+  {
+    "Country": "United States",
+    "CountryCode": "US",
+    "Type": "Rosé",
+    "Count": 8,
+    "Price": 18.86,
+    "Rating": 8.68
+  },
+  {
+    "Country": "United States",
+    "CountryCode": "US2",
+    "Type": "Red Wine",
+    "Count": 555,
+    "Price": 50.72,
+    "Rating": 8.26
+  },
+  {
+    "Country": "United States",
+    "CountryCode": "US3",
+    "Type": "Champagne",
+    "Count": 28,
+    "Price": 26.7,
+    "Rating": 8.12
+  },
+  {
+    "Country": "United States",
+    "CountryCode": "US4",
+    "Type": "White Wine",
+    "Count": 143,
+    "Price": 20.37,
+    "Rating": 8.02
+  }
+  
+];
